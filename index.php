6ece42ac3d06fca86b55b16b7fc56e47ddf34c6e
TheAnh3734 <55874840+TheAnh3734@users.noreply.github.com>
2019-10-09 14:33:22 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,36 +1,40 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>php page</title>
-</head>
 <body>
-	<h1>first php page</h1>
-	<?php
-	echo "show all rows from postgres database";
-	$db = parse_url(getenv("DATABASE_URL"));
 
-     $pdo = new PDO("pgsql:" . sprintf(
-    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
-    $db["host"],
-    $db["port"],
-    $db["user"],
-    $db["pass"],
-    ltrim($db["path"], "/")
-));
-$sql = "select studentname, course from resistercourse";
-$stmt = $pdo ->prepare($sql);
-$stmt->setFetchMode(PDO::ASSOC);
-$stmt->execute();
-$resultSet = $stmt->fetchAll();
+<h1>My first PHP page</h1>
+
+<?php
+	echo "Show all rows from Postgres Database";
+	
+	//Refere to database 
+	$db = parse_url(getenv("DATABASE_URL"));
+	$pdo = new PDO("pgsql:" . sprintf(
+	    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+	    $db["host"],
+	    $db["port"],
+	    $db["user"],
+	    $db["pass"],
+	    ltrim($db["path"], "/")
+	));
+	//you sql query
+	$sql = "SELECT studentname, course FROM registercourse";
+	$stmt = $pdo->prepare($sql);
+	//execute the query on the server and return the result set
+	$stmt->setFetchMode(PDO::FETCH_ASSOC);
+	$stmt->execute();
+	$resultSet = $stmt->fetchAll();
+	//display the data 
+?>
+<ul>
+	<?php
+		foreach ($resultSet as $row) {
+			echo "<li>" .
+				$row["studentname"] . '--'. $row["course"]
+			. "</li>";
+		}
 	?>
-	<ul>
-		<?php
-		foreach($resultSet as $row){
-		echo "<li>" .
-		$row["studentname"] . '--'. $row["course"]. "</li>";
-	}
-		?>
-	</ul>
+</ul>
 
 </body>
 </html>
