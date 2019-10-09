0c70957cde9551b03f76d780dc6fb61f55535940
TheAnh3734 <55874840+TheAnh3734@users.noreply.github.com>
2019-10-09 14:15:14 +0700

    Update registers.php

--- registers.php
+++ registers.php
@@ -4,44 +4,45 @@
 	<title></title>
 </head>
 <body>
-<form method="post" action="doRegister.php">
-	<table>
-		<tr>
-			<td>name</td>
-			<td><input type="text" name="txtName"></td>
-		</tr>
-		<tr>
-			<td>country</td>
-			<td>
-				<select name="cbCourse">
-					<option value="C#">C#</option>
-					
-					<option value="Java">java</option>
-					<option value="Cloud">clould</option>
-					
-				</select>
-			</td>
-		</tr>
-		<tr>
-			<td>data of birth</td>
-			<td><input type="date" name="dob"/></<td>
-		</tr>
-		<tr>
-			<td>gender</td>
-			<td>
-				<input type="radio" name="gender" value="male">male
-				<input type="radio" name="gender" value="female">female
-			</td>
-		</tr>
-		<tr>
-			<td>favorites</td>
-			<td>
-				<input type="checkbox" name="book" value="book">book
-				<input type="checkbox" name="music" value="book">music
-          </td>
-		</tr>
-	</table>
-	
-</form>
+	<form method="post" action="doRegister.php">
+		<table>
+			<tr>
+				<td>Name</td>
+				<td><input type="text" name="txtName"></td>
+			</tr>
+			<tr>	
+				<td>Course</td>
+				<td>
+					<select name="cbCourse">
+						<option value="C#">C#</option>
+						<option value="Java">Java</option>
+						<option value="Cloud"> Cloud</option>
+					</select>
+				</td>
+			</tr>
+			<tr>
+				<td>Date of Birth</td>
+				<td><input type="Date" name="dob"/></td>
+			</tr>
+			<tr>
+				<td>Gender</td>
+				<td>
+					<input type="radio" name="gender" value="Male">Male
+					<input type="radio" name="gender" value="Female">Female
+				</td>
+			</tr>
+			<tr>
+				<td>Favorites</td>
+				<td>
+					<input type="checkbox" name="book" value="book">Books
+					<input type="checkbox" name="car" value="car">Cars
+				</td>
+			</tr>
+			<tr>
+				<td></td>
+				<td><input type="submit" name="Register"></td>
+			</tr>
+		</table>
+	</form>
 </body>
 </html>
